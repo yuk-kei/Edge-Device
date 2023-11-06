6708eff352d3ed9dc551a03dbd55b68d5246dbb8
Yukkei Ho <yukkei_mh@outlook.com>
2023-11-06 12:04:14 -0800

    Refract for single device only

--- Multiple/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-REGISTRATOR_URL = os.environ.get('REGISTRATOR_URL')
-DATA_WIZARD_PORT = os.environ.get('DATA_WIZARD_PORT')
-SYS_CONTROL_PORT = os.environ.get('SYS_CONTROL_PORT')
-PRODUCER_CONF = {'bootstrap.servers': os.environ.get('KAFKA_BOOTSTRAP_SERVERS')}
-TOPIC_NAME = os.environ.get('TOPIC_NAME')
--- Single/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-REGISTRATOR_URL = os.environ.get('REGISTRATOR_URL', "http://128.195.151.182:9002/api/devices/register")
-PRODUCER_CONF = {'bootstrap.servers': os.environ.get('KAFKA_BOOTSTRAP_SERVERS')}
-TOPIC_NAME = os.environ.get('TOPIC_NAME')
